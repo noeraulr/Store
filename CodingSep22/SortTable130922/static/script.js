e1fa2abc26eee7d109da14615d678653a302f102
noeraulr <noeraulr1@gmail.com>
2022-09-21 20:45:51 -0500

    DataVisualization Sorted Table

---
+++ CodingSep22/SortTable130922/static/script.js
@@ -0,0 +1,99 @@
+const tableContent = document.getElementById("table-content")
+
+const createRow = (obj) => {
+  const row = document.createElement("tr");
+  const objKeys = Object.keys(obj);
+  objKeys.map((key) => {
+    const cell = document.createElement("td");
+    cell.setAttribute("data-attr", key);
+    cell.innerHTML = obj[key];
+    row.appendChild(cell);
+  });
+  return row;
+};
+
+
+const getTableContent = (data) => {
+  data.map((obj) => {
+    const row = createRow(obj);
+    tableContent.appendChild(row);
+  });
+};
+
+window.addEventListener("load", () => {
+  getTableContent(response.pokedata);
+});
+
+const tableButtons = document.querySelectorAll("th button");
+
+const sortData = (data, param, direction = "asc") => {
+  tableContent.innerHTML = '';
+  const sortedData =
+    direction == "asc"
+      ? [...data].sort(function (a, b) {
+          if (a[param] < b[param]) {
+            return -1;
+          }
+          if (a[param] > b[param]) {
+            return 1;
+          }
+          return 0;
+        })
+      : [...data].sort(function (a, b) {
+          if (b[param] < a[param]) {
+            return -1;
+          }
+          if (b[param] > a[param]) {
+            return 1;
+          }
+          return 0;
+        });
+
+  getTableContent(sortedData);
+};
+
+window.addEventListener("load", () => {
+  getTableContent(response.pokedata);
+
+  [...tableButtons].map((button) => {
+    button.addEventListener("click", (e) => {
+
+      if (e.target.getAttribute("data-dir") == "desc") {
+        sortData(response.pokedata, e.target.id, "desc");
+        e.target.setAttribute("data-dir", "asc");
+      } else {
+        sortData(response.pokedata, e.target.id, "asc");
+        e.target.setAttribute("data-dir", "desc");
+      }
+
+    });
+  });
+});
+const resetButtons = (event) => {
+  [...tableButtons].map((button) => {
+    if (button !== event.target) {
+      button.removeAttribute("data-dir");
+    }
+  });
+};
+
+window.addEventListener("load", () => {
+  getTableContent(response.pokedata);
+
+  [...tableButtons].map((button) => {
+    button.addEventListener("click", (e) => {
+
+      resetButtons(e);
+
+      if (e.target.getAttribute("data-dir") == "desc") {
+        sortData(response.pokedata, e.target.id, "desc");
+        e.target.setAttribute("data-dir", "asc");
+      } else {
+        sortData(response.pokedata, e.target.id, "asc");
+        e.target.setAttribute("data-dir", "desc");
+      }
+
+    });
+  });
+});
+
